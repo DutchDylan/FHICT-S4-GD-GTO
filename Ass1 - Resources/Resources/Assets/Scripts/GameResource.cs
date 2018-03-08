3fc5c9a1ca53aa93b4656bd003830bc62a9aaf89
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-08 11:25:54 +0100

    Thing

---
+++ Ass1 - Resources/Resources/Assets/Scripts/GameResource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResource : MonoBehaviour
+{
+    private string rName;
+    private float rAmount;
+
+    public GameResource(string name)
+    {
+        rName = name;
+        rAmount = 0;
+    }
+    public GameResource(string name, float amount)
+    {
+        rName = name;
+        rAmount = amount;
+    }
+}
