436871a74595ea9273a4543de6718f5a6cca1380
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-09 21:32:33 +0100

    Classical Assignment end result so far

--- Ass2 - Grid and Units/Grid and Units/Assets/Scripts/UnitFactoryPP.cs
+++ Ass2 - Grid and Units/Grid and Units/Assets/Scripts/UnitFactoryPP.cs
@@ -7,15 +7,27 @@ public class UnitFactoryPP : MonoBehaviour
     public UnitPP Prototype;
     public MapPP Map;
 
+    public ResourcePP Resource;
+    public int Cost;
+
     public int x;
     public int y;
 
-    public void SpawnUnit(UnitPP unit)
+    public void SpawnUnit(/* UnitPP unit */)
     {
+        if(ResourcePP.CanAfford(Cost))
+        {
+            Resource.RemoveAmount(Cost);
+            UnitPP newUnit = Instantiate(Prototype);
+            CellPP cell = Map.GetCell(x, y);
+
+            newUnit.transform.SetParent(cell.transform, false);
+        }
+
         UnitPP newUnit = Instantiate(Prototype);
         CellPP CellPP = Map.GetCell(x, y);
 
-        newUnit.transform.SetParent(CellPP.transform)
+        newUnit.transform.SetParent(CellPP.transform);
     }
     public void SpawnSpearman()
     {
