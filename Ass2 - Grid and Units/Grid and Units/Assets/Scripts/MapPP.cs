a296949d222937c14fd8d526fa96ebfa433f146b
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-09 12:30:04 +0100

    Mid Classical Assigment

---
+++ Ass2 - Grid and Units/Grid and Units/Assets/Scripts/MapPP.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPP : MonoBehaviour
+{
+    public int SizeX;
+    public int SizeY;
+
+    public float TileOffset;
+
+    public CellPP Prototype;
+
+    //public List<Cell> Cells;
+
+    public void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        float xOffset = 0;
+        for (int i = 0; i < SizeX; i++)
+        {
+            float yOffset = 0;
+            for (int j = 0; i < SizeY; i++)
+            {
+                CellPP newTile = Instantiate(Prototype);
+                newTile.transform.SetParent(transform);
+                newTile.transform.localPosition = new Vector3(xOffset, 0, yOffset);
+                newTile.name = string.Format("Cell {0}x{1}",i,j);
+
+                newTile.X = i;
+                newTile.Y = j;
+
+                yOffset += TileOffset;
+            }
+            xOffset += TileOffset;
+        }
+    }
+    
+    [ContextMenu("Test Cell 3,4")]
+    public void Test()
+    {
+        CellPP test = GetCell(3, 4);
+        Debug.Log(test.name, test);
+    }
+
+    public CellPP GetCell(int x, int y)
+    {
+        //CellPP()
+        //List<CellPP>
+        //Cell[,]
+        if (x > SizeX || y == SizeY)
+        {
+            Debug.Log("Out of Bounds");
+            return null;
+        }
+
+        return transform.GetChild((x * SizeX) + y ).GetComponent<CellPP>();
+    }
+
+}
---
+++ Ass2 - Grid and Units/Grid and Units/Assets/Scripts/UnitFactoryPP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFactoryPP : MonoBehaviour
+{
+    public UnitPP Prototype;
+    public MapPP Map;
+
+    public int x;
+    public int y;
+
+    public void SpawnUnit(UnitPP unit)
+    {
+        UnitPP newUnit = Instantiate(Prototype);
+        CellPP CellPP = Map.GetCell(x, y);
+
+        newUnit.transform.SetParent(CellPP.transform)
+    }
+    public void SpawnSpearman()
+    {
+
+    }
+}
