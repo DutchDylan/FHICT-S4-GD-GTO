8a565b092f4e8bc8d80bf4021b674c7fb422edb3
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-09 11:18:24 +0100

    Ass 2 Start

---
+++ Ass2 - Grid and Units/Grid and Units/Assets/Scripts/GameResource.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResource : MonoBehaviour
+{
+    private string rName;
+    private float rAmount;
+
+    public GameResource(string name)
+    {
+        rName = name;
+        rAmount = 0;
+    }
+    public GameResource(string name, float amount)
+    {
+        rName = name;
+        rAmount = amount;
+    }
+
+    public void Increase()
+    {
+        rAmount++;
+    }
+    public void Increase(int amount )
+    {
+        rAmount = rAmount + amount;
+    }
+
+    public void Decrease()
+    {
+        rAmount--;
+    }
+    public void Decrease(int amount)
+    {
+        rAmount = rAmount - amount;
+    }
+
+    public override string ToString()
+    {
+        return rName + ": " + rAmount;
+    }
+}
