637b076ce7b8a5ddfcc24ad31665ead0e65ff124
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-15 11:37:45 +0100

    Player Implementations

--- Ass3a - Independent Players/ResourcesMulti/Assets/Scripts/GameManager.cs
+++ Ass3a - Independent Players/ResourcesMulti/Assets/Scripts/GameManager.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    private List<Player> players;
+    private Player currentPlayer;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        //Player playa = new Player(0, "Henk");
+        //players.Add(playa);
+        players.Add(new Player(0, "Player One"));
+        players.Add(new Player(1, "Player Two"));
+        players.Add(new Player(2, "Player Three"));
+        players.Add(new Player(3, "Player Four"));
+        currentPlayer = players[0];
+    }
+
+    public void NextPlayer() 
+    {
+        int id = currentPlayer.pID + 1;
+        if (players[id] != null)
+        {
+            currentPlayer = players[id];
+        }
+        else
+        {
+            players[id] = players[0];
+        }
+    }
+
+    	
+
 }
--- Ass3a - Independent Players/ResourcesMulti/Assets/Scripts/Player.cs
+++ Ass3a - Independent Players/ResourcesMulti/Assets/Scripts/Player.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int pID;
+    public int pID { get; private set; }
     private string pName;
-    private List<GameResource> pResources;
+    private List<GameResource> pResources; //Specifically for non-unit resources
+    private List<GameResource> pInventory; //Specifically for units
 
-
+    public Player(int id, string name)
+    {
+        pID = id;
+        pName = name;
+        pResources = new List<GameResource>();
+        pInventory = new List<GameResource>();
+    }
+    public Player(int id, string name, List<GameResource> resources, List<GameResource> inventory)
+    {
+        pID = id;
+        pName = name;
+        pResources = resources;
+        pInventory = inventory;
+    }
+    
 
 }
