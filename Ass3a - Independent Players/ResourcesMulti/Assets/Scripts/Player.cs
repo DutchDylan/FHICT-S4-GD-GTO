13f933e5d569eed04de25313b0f423229dc4e491
Dylan de Wolff <jaxwolff@avans.nl>
2018-03-15 09:42:09 +0100

    First step towards player class

---
+++ Ass3a - Independent Players/ResourcesMulti/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int pID;
+    private string pName;
+    private List<GameResource> pResources;
+
+
+
+}
